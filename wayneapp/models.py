7911b5059d2ebe9a0e11333c13ebeeb0eb37e688
Lars Leithold <lars.leithold@project-a.com>
2019-11-22 11:59:08 +0100

    added GenericModel

--- wayneapp/models.py
+++ wayneapp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
+from model_utils.models import TimeStampedModel
 
-# Create your models here.
+
+class GenericModel(TimeStampedModel):
+    key = models.TextField(null=False)
+    version = models.IntegerField(null=False, default=1)
+    data = JSONField(null=False)
+
+    class Meta:
+        abstract = True
+        unique_together = ('key', 'version')
+
+
+# automatically generate generic classes based on json schema definitions:
+# $ python manage.py generate_classes
+# ########################### start generic classes ###########################
+
+
+class Address(GenericModel):
+    pass
+
+
+class Product(GenericModel):
+    pass
+
+
+class User(GenericModel):
+    pass
+
+
+# ########################### end generic classes #############################
