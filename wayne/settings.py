762fd1fbbd7a9f7cbf212817d3a744e5a78fa119
Christos Athinaiou <christos.athinaiou@project-a.com>
2019-11-22 11:23:11 +0100

    postgres docker compose

--- wayne/settings.py
+++ wayne/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'wayne.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wayne',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
