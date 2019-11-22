2fe7ebbfa123eb7383e38b0fe64f84c54fa199b8
Lars Leithold <lars.leithold@project-a.com>
2019-11-22 12:02:52 +0100

    added update_or_create_generic_model

---
+++ wayneapp/services.py
@@ -0,0 +1,18 @@
+import importlib
+
+from wayneapp.models import GenericModel
+
+
+def update_or_create_generic_model(type_name: str, key: str, version: int, data: str) -> (GenericModel, bool):
+    models_module = importlib.import_module("polls.models")
+    generic_class = getattr(models_module, str(type_name))
+
+    return generic_class.objects.update_or_create(
+        key=key,
+        version=version,
+        defaults={
+            'key': key,
+            'version': version,
+            'data': data
+        }
+    )
