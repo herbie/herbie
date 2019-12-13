c738dd671306b898b2761000270cd4c8035cb47e
Gayane Arshakyan <46559869+gayane-projectA@users.noreply.github.com>
2019-12-13 14:49:37 +0100

    add delete by key fail/success tests (#87)
    * add delete by key fail/success tests

--- wayneapp/tests/test_controller.py
+++ wayneapp/tests/test_controller.py
@@ -79,3 +79,62 @@ class TestBusinessEntityController(TestCase):
             response = client.post('/api/test_entity/delete', data, format='json')
 
             self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    @patch.object(BusinessEntityManager, 'delete', side_effect=Exception('Test'))
+    @patch.object(DeleteBusinessEntityController, 'has_delete_permission', return_value=False)
+    def test_delete_business_entity_should_fail_unauthorized(self, mock_manager, mock_controller):
+        data = {
+            'version': 'v1',
+            'key': 'x-id',
+        }
+
+        client = APIClient()
+        client.force_authenticate(user=None)
+        response = client.post('/api/test_entity/delete', data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.data, {'message': 'unauthorized'})
+
+    @patch.object(BusinessEntityManager, 'delete_by_key', return_value=2)
+    @patch.object(DeleteBusinessEntityController, 'has_delete_permission', return_value=True)
+    def test_delete_business_entity_all_versions_success(self, mock_manager, mock_controller):
+        data = {
+            'key': 'x-id',
+        }
+
+        client = APIClient()
+        client.force_authenticate(user=None)
+        response = client.post('/api/test_entity/delete', data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {'message': 'entity with key x-id deleted from all versions'})
+
+
+    @patch.object(BusinessEntityManager, 'delete_by_key', return_value=0)
+    @patch.object(DeleteBusinessEntityController, 'has_delete_permission', return_value=True)
+    def test_delete_business_entity_all_versions_fail(self, mock_manager, mock_controller):
+        data = {
+            'key': 'x-id',
+        }
+
+        client = APIClient()
+        client.force_authenticate(user=None)
+        response = client.post('/api/test_entity/delete', data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {'message': 'entity with key x-id not found'})
+
+
+    @patch.object(BusinessEntityManager, 'delete_by_key', return_value=2)
+    @patch.object(DeleteBusinessEntityController, 'has_delete_permission', return_value=False)
+    def test_delete_business_entity_all_versions_fail_unauthorized(self, mock_manager, mock_controller):
+        data = {
+            'key': 'x-id',
+        }
+
+        client = APIClient()
+        client.force_authenticate(user=None)
+        response = client.post('/api/test_entity/delete', data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.data, {'message': 'unauthorized'})
