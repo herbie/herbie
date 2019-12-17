ee0c97b11b2a088ddac502cdd10bf9282c631257
Gayane Arshakyan <46559869+gayane-projectA@users.noreply.github.com>
2019-12-17 17:38:03 +0100

    add json schema validator tests (#92)
    * add json schema validator tests

---
+++ wayneapp/tests/services/test_json_schema_validator.py
@@ -0,0 +1,81 @@
+from django.test import TestCase
+from wayneapp.services import settings
+from wayneapp.services import JsonSchemaValidator
+
+
+class JsonSchemaValidatorTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(JsonSchemaValidatorTestCase, cls).setUpClass()
+        settings.SCHEMA_PACKAGE_NAME = 'wayneapp.tests.test_schema'
+        cls._schema_validator = JsonSchemaValidator()
+        cls._business_entity = 'test_entity'
+        cls._version_1 = 'v1'
+
+    def test_validate_success(self):
+        json_data = {
+            "testId": 12,
+            "name": "testName"
+        }
+
+        validation_messages = self._schema_validator.validate_schema(json_data, self._business_entity, self._version_1)
+
+        self.assertEqual({}, validation_messages)
+
+    def test_validate_additional_property_error(self):
+        json_data = {
+            "testId": 12,
+            "name": "testName",
+            "someAdditionalProperty": "testSome"
+        }
+
+        validation_messages = self._schema_validator.validate_schema(json_data, self._business_entity, self._version_1)
+        message_response_expected = {'additionalProperties':
+                                         {'error_message': 'Additional properties are not '
+                                                           "allowed ('someAdditionalProperty' "'was unexpected)',
+                                          'validation_error': 'additionalProperties'}
+                                     }
+
+        self.assertEqual(message_response_expected, validation_messages)
+
+    def test_validate_required_error(self):
+        json_data = {
+            "name": "testName",
+        }
+
+        validation_messages = self._schema_validator.validate_schema(json_data, self._business_entity, self._version_1)
+        message_response_expected = {'testId':
+            {
+                'error_message': "'testId' is a required property",
+                'validation_error': 'required'
+            }
+        }
+
+        self.assertEqual(message_response_expected, validation_messages)
+
+    def test_validate_type_error(self):
+        json_data = {
+            "testId": "wrongType",
+            "name": "testName",
+        }
+
+        validation_messages = self._schema_validator.validate_schema(json_data, self._business_entity, self._version_1)
+        message_response_expected = {'testId':
+            {
+                'error_message': "'wrongType' is not of type 'integer'",
+                'validation_error': 'integer'
+            }
+        }
+
+        self.assertEqual(message_response_expected, validation_messages)
+
+    def test_validate_entity_in_version_missing(self):
+        json_data = {
+            "testId": "wrongType",
+            "name": "testName",
+        }
+
+        validation_messages = self._schema_validator.validate_schema(json_data, 'wrong', self._version_1)
+        message_response_expected = "version " + self._version_1 + " does not exist"
+
+        self.assertEqual(message_response_expected, validation_messages)
