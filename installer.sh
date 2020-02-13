b434dfaa61d02f1e7105f3e55198a5fadd79f42a
Christoph Rieß <riess@carlfinance.de>
2020-02-13 15:43:15 +0100

    [add] installer.sh to be called in docker image build step (can add private code from extra_packages folder to python project)

---
+++ installer.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+EXTRA_PACKAGES_LOCATION='./extra_packages'
+REQUIREMENTS_FILE='requirements.txt'
+RESULT=0
+
+if [ -d "$EXTRA_PACKAGES_LOCATION" ]; then
+  if [ -n "$(ls $EXTRA_PACKAGES_LOCATION)" ]; then
+    pip install $EXTRA_PACKAGES_LOCATION/*
+    RESULT=$?
+  else
+    echo "no extra packages found (in 'extra_packages')"
+  fi
+else
+  echo "'extra_packages' folder not existing … skipping"
+fi
+
+pip install -r $REQUIREMENTS_FILE
+
+RESULT=$(( RESULT + $? ))
+
+exit $RESULT
