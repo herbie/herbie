951c67058e66fb9caf7eb97a2b4f9af00e47af44
tobias.rosenstock <tobi.rosenstock@gmail.com>
2020-05-13 13:44:06 +0200

    changing package dependencies

--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
 from setuptools import setup
 
-setup(
-    name='herbie',
-    version='0.1.0',
-    packages=['herbie'],
-    url='https://github.com/herbie/herbie',
-    license='MIT',
-    author='',
-    author_email='',
-    description='Herbie',
-    setup_requires=['setuptools_scm'],
-    include_package_data=True,
-    install_requires=["django>=2.2"],
-)
+setup()
