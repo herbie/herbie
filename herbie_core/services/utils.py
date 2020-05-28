67716880b9cb2f5d48ba5eefcc4af5b0fea48c0f
tobias.rosenstock <tobi.rosenstock@gmail.com>
2020-05-28 15:33:48 +0200

    WIP

--- herbie_core/services/utils.py
+++ herbie_core/services/utils.py
@@ -1,5 +1,5 @@
 import re
-
+import importlib
 from django.conf import settings
 from herbie_core.models import AbstractBusinessEntity
 
@@ -14,6 +14,7 @@ class BusinessEntityUtils:
     def camel_to_snake(word: str) -> str:
         # explanation: https://stackoverflow.com/a/1176023
         interim = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', word)
+
         return re.sub('([a-z0-9])([A-Z])', r'\1_\2', interim).lower()
 
     @staticmethod
@@ -22,4 +23,6 @@ class BusinessEntityUtils:
 
     @staticmethod
     def get_entity_class(entity_type_name: str):
-        return getattr(settings.APP_LABEL + 'models', BusinessEntityUtils.snake_to_camel(entity_type_name))
+        model_module = importlib.import_module(settings.APP_LABEL + 'models')
+
+        return getattr(model_module, BusinessEntityUtils.snake_to_camel(entity_type_name))
