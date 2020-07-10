12728edaf2784d2e79a90c1a5f7708f9b11831d2
Christoph Rieß <riess@carlfinance.de>
2020-07-10 15:29:23 +0200

    [fix] add dependency and code to check formats on strings according to http://json-schema.org/understanding-json-schema/reference/string.html#id8

--- herbieapp/services/json_schema_validator.py
+++ herbieapp/services/json_schema_validator.py
@@ -2,7 +2,7 @@ import json
 import re
 
 from herbieapp.services import SchemaRegistry
-from jsonschema import Draft7Validator
+from jsonschema import Draft7Validator, draft7_format_checker
 from herbieapp.constants import ValidatorResponseConstants, ControllerConstants
 
 
@@ -14,7 +14,7 @@ class JsonSchemaValidator:
         if not self.version_exist(version, business_entity):
             return ControllerConstants.VERSION_NOT_EXIST.format(version)
         schema = self._get_json_schema(business_entity, version)
-        data_validated = Draft7Validator(schema)
+        data_validated = Draft7Validator(schema, format_checker=draft7_format_checker)
         sorted_errors = sorted(data_validated.iter_errors(json_data), key=lambda e: e.path)
         errors = {}
 
