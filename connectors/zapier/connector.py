8068b6b95e7677f6e503832309e2d66f7050c36f
Christoph Rieß <riess@carlfinance.de>
2020-07-23 11:50:55 +0200

    [add] connector for transmitting business objects to zapier

---
+++ connectors/zapier/connector.py
@@ -0,0 +1,49 @@
+import json
+import os
+import sys
+import http.client
+
+from time import sleep
+from kafka import KafkaConsumer, errors
+from utils import *
+
+import logging
+logging.basicConfig(format='[%(asctime)s] %(levelname)s %(message)s')
+
+entity_name = 'funnel_exec'
+
+starting = 0
+
+while starting < 3:
+    starting += 1
+    try:
+        consumer = KafkaConsumer(bootstrap_servers=os.environ['KAFKA_URL'],
+                                 value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+        consumer.subscribe([entity_name])
+        break
+    except errors.NoBrokersAvailable:
+        logging.warning(f'NoBrokersAvailable retry {starting}')
+        sleep(2)
+        continue
+
+logging.info(f'start reading messages from topic: {entity_name}')
+# The iterator will be blocked forever, because we didn't set a consumer_timeout_ms parameter
+# in the KafkaConsumer. So we should continuously receive any new messages.
+for consumer_record in consumer:
+    message = consumer_record.value
+    key = message['key']
+    action = message['action']
+    try:
+        if action == 'create':
+            zapier_params = json.dumps(map_message_to_zapier(entity_name, message['payload'])).encode('ascii')
+            host, path = PRODUCT_ID_TO_HOOK[message['payload']['product_id']].split('/')
+            connection = http.client.HTTPSConnection(host, 443)
+            connection.request('POST', '/' + path, zapier_params, {'Content-Type': 'application/json'})
+            logging.info(f'{connection.getresponse().read().decode()}')
+        else:
+            logging.warning(f'zapier connector is not processing: {action} for {entity_name} messages')
+    except Exception as e:
+        if 'payload' in message:
+            logging.error(f'unable to consume {message["payload"]}; ({type(e).__name__}: {e})')
+        else:
+            logging.error(f'unable to process message: {message} ({type(e).__name__}: {e})')
---
+++ connectors/zapier/utils/__init__.py
@@ -0,0 +1,129 @@
+from datetime import datetime
+from uuid import uuid4
+
+MAPPING = {
+    '_ga': 'ga_session_id',
+    '_gid': 'ga_user_id',
+    'adgroupid': 'ga_adgroup_id',
+    'age': 'person_age',
+    'ageTax': 'is_person_age_55',
+    'ageTax_de': 'is_person_age_55_de',
+    'appFunnel': '', # what to map from?
+    'carlId': 'carl_id',
+    'corporateForm': 'company_corporate_form',
+    'dateActivity': 'lead_set_activity_at',
+    'degree': 'person_degree',
+    'degree_de': 'person_degree_de',
+    'dependencyOwner': 'company_dependency_owner',
+    'dependencyOwner_de': 'company_dependency_owner_de',
+    'device': 'ga_device',
+    'digitalizationChallenge1': 'digitalization_challenge_question_1',
+    'digitalizationChallenge1_de': 'digitalization_challenge_question_1_de',
+    'dynamickey': 'ga_dynamickey',
+    'ebitMultiple': 'company_ebit_multiple',
+    'email': 'person_email',
+    'employees': 'company_count_employees_range',
+    'evavg': 'company_enterprise_value_avg',
+    'evavg-Carl': '', # what to map from
+    'evavgNoCarlRounded': 'company_enterprise_value_avg_80_rounded',
+    'evavgRounded': 'company_enterprise_value_avg_rounded',
+    'evmax': 'company_enterprise_value_max',
+    'evmax-Carl': '', #what to map from
+    'evmaxNoCarlRounded': 'company_enterprise_value_max_80_rounded',
+    'evmaxRounded': 'company_enterprise_value_max_rounded',
+    'evmin': 'company_enterprise_value_min',
+    'evmin-Carl': '', # what to map from
+    'evminNoCarlRounded': 'company_enterprise_value_min_80_rounded',
+    'evminRounded': 'company_enterprise_value_min_rounded',
+    'familyBusiness': 'is_company_family_business',
+    'familyBusiness_de': 'is_company_family_business_de',
+    'financingChallenge': 'digitalization_challenge_question_2',
+    'financingChallenge_de': 'digitalization_challenge_question_2_de',
+    'firstname': 'person_first_name',
+    'focus': 'person_focus',
+    'focus_de': 'person_focus_de',
+    'foundingDate': 'company_founding_time_range',
+    'funnelID': '', # what to map from
+    'gclid': 'ga_gclid',
+    'gender': 'person_gender',
+    'keyword': 'ga_keyword',
+    'largestCustomer': 'company_share_largest_customer',
+    'lastname': 'person_last_name',
+    'matchtype': 'ga_matchtype',
+    'name': 'person_name',
+    'network': 'ga_network',
+    'numberCustomer': 'company_number_customer_range',
+    'pageID': 'page_id',
+    'phone': 'person_phone',
+    'productID': 'product_id',
+    'profit': 'company_ebit',
+    'revenue': 'company_revenue',
+    'revenueMultiple': 'company_revenue_multiple',
+    'saleDate': 'company_sale_date',
+    'saleDate_de': 'company_sale_date_de',
+    'salePrice': 'company_sale_price',
+    'sector': 'company_industry',
+    'sector_de': 'company_industry_de',
+    'shareholderType': 'is_person_type',
+    'shareholderType_de': 'is_person_type_de',
+    'successionAim': 'company_sale_reason',
+    'successionAim_de': 'company_sale_reason_de',
+    'successorType': 'company_sale_target',
+    'successorType_de': 'company_sale_target_de',
+    'timeHolding': 'company_duration_stake_holding',
+    'timeHolding_de': 'company_duration_stake_holding',
+    'timestamp': 'lead_created_at',
+    'utm_campaignid': 'utm_campaign_id',
+    'windowURL': 'window_url',
+}
+
+PRODUCT_ID_TO_HOOK = {
+    'spk_rating_long': 'hooks.zapier.com/hooks/catch/2517134/o6beprz/',
+    'standalone_rating_long': 'hooks.zapier.com/hooks/catch/2517134/o6beprz/',
+    'rating_long': 'hooks.zapier.com/hooks/catch/2517134/o6beprz/',
+    'standalone_rating_short': 'hooks.zapier.com/hooks/catch/2517134/o6beprz/',
+    'rating_short': 'hooks.zapier.com/hooks/catch/2517134/o6beprz/',
+    'standalone_sell': 'hooks.zapier.com/hooks/catch/2517134/odbs7bw/',
+    'sell': 'hooks.zapier.com/hooks/catch/2517134/odbs7bw/',
+    'standalone_tax': 'hooks.zapier.com/hooks/catch/2517134/o3jq1of/',
+    'tax':  'hooks.zapier.com/hooks/catch/2517134/o3jq1of/',
+}
+
+def current_ts():
+    return datetime.now().isoformat()
+
+def map_message_to_zapier(entity_name, message):
+    mapped = {
+        'intent': '',
+        'intent_de': '',
+    }
+
+    for orig_k, v in message.items():
+        if type(v) == str:
+            # special handling for intent fields
+            if orig_k == 'valution_intent' or orig_k == 'tax_intent':
+                mappend['intent'] += v
+
+            if orig_k == 'valution_intent_de' or orig_k == 'tax_intent_de':
+                mappend['intent_de'] += v
+
+        do_map = None
+        for to_k, from_k in MAPPING.items():
+            if from_k == orig_k:
+                do_map = to_k
+                break
+        if do_map:
+            mapped[to_k] = v
+        else:
+            mapped[orig_k] = v
+
+    return mapped
+
+
+def zapier_request_business_object(zapier_response):
+    return {
+        'key': uuid4(),
+        'data': normalize_to_zapier_object(zapier_response),
+        'version': 1,
+    }
+
