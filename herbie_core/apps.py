eb26fc8c1e0b4adfd439a732c0ce7bee6bf179ed
Rui Ribeiro <ruimbribeiro@gmail.com>
2020-07-27 11:18:58 +0100

    fix import

--- herbie_core/apps.py
+++ herbie_core/apps.py
@@ -6,7 +6,7 @@ class HerbieCoreConfig(AppConfig):
     verbose_name = "HerbieCore"
 
     def ready(self):
-        from herbie_core.services.message_publisher import Registry
+        from herbie_core.services.message_publisher.message_publisher import Registry
         from herbie_core.services.message_publisher.logging_publisher import LoggingPublisher
 
         Registry.add_publisher(LoggingPublisher())
