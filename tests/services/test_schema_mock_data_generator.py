3c3f75db67d192438522382ae0049ec9da2e05e9
Rui Ribeiro <ruimbribeiro@gmail.com>
2020-09-09 14:02:27 +0200

    add some json schemas to generate mock data from

--- tests/services/test_schema_mock_data_generator.py
+++ tests/services/test_schema_mock_data_generator.py
@@ -1,23 +1,27 @@
+import glob
 import json
+import os
 import pkgutil
-from django.test import TestCase
+
+import pytest
 from herbie_core.services.schema_mock_data.schema_mock_data_generator import SchemaMockDataGenerator
 from jsonschema import validate, Draft7Validator, SchemaError
 
 
-class TestSchemaMockDataGenerator(TestCase):
-    def test_mock_generation(self):
-        schema = self._load_test_schema()
+class TestSchemaMockDataGenerator:
+    os.chdir("tests/test_schema/mock_data_generation")
+    mock_data_json_schemas = glob.glob("*.json")
+
+    @pytest.mark.parametrize("filename", mock_data_json_schemas)
+    def test_mock_generation(self, filename):
+        schema = json.loads(pkgutil.get_data("tests.test_schema", "mock_data_generation/" + filename))
 
         try:
             Draft7Validator.check_schema(schema)
         except SchemaError as e:
-            self.fail(f"Schema is not a valid DRAFT 7 JSOn Schema, please fix it! {repr(e)}")
+            pytest.fail(f"Schema is not a valid DRAFT 7 JSOn Schema, please fix it! {repr(e)}")
 
         generator = SchemaMockDataGenerator()
         mock_data_for_json_schema = generator.mock_data_from_schema(schema)
 
         validate(mock_data_for_json_schema, schema)
-
-    def _load_test_schema(self):
-        return json.loads(pkgutil.get_data("tests.test_schema", "test_entity/test_entity_v1.json"))
