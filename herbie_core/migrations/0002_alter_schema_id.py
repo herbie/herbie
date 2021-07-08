c93b2d76979257894b55451a5f62e246326c7164
Christoph Rieß <riess@carlfinance.de>
2021-07-08 09:27:12 +0200

    [fix] `black` code formatting for new migration file

--- herbie_core/migrations/0002_alter_schema_id.py
+++ herbie_core/migrations/0002_alter_schema_id.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('herbie_core', '0001_initial'),
+        ("herbie_core", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='schema',
-            name='id',
-            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+            model_name="schema",
+            name="id",
+            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID"),
         ),
     ]
